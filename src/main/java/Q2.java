01d9c7c91534110e26fd789f34542c424e77acf8
samangishamim <samangi624@gmail.com>
2024-02-15 22:49:28 +0330

    create 4 class for test

---
+++ src/main/java/Q2.java
@@ -0,0 +1,6 @@
+public class Q2 {
+    public static void main(String[] args) {
+        Anagram.AnagramTest();
+    }
+
+}
---
+++ src/main/java/Q4.java
@@ -0,0 +1,24 @@
+public class Q4 {
+    public static void main(String[] args) {
+        MyHashMap myHashMap=new MyHashMap<>();
+        myHashMap.put("A", "Apple");
+        myHashMap.put("A","APPLE2");
+        myHashMap.put("B", "Banana");
+        myHashMap.put("C", "Cherry");
+        myHashMap.put("E", "Cherry");
+
+
+        System.out.println("Contains key 'A': " + myHashMap.containsKey("A"));
+        System.out.println();
+        System.out.println("Is HashMap empty: " + myHashMap.isEmpty());
+        System.out.println();
+        System.out.println("All values: " + myHashMap);
+        System.out.println("Value for key 'B': " + myHashMap.get("B"));
+
+        System.out.println();
+        myHashMap.replace("A", "wow");
+        System.out.println("Value for key 'A' after replacement: " + myHashMap.get("A"));
+        System.out.println("All values: " + myHashMap);
+    }
+}
+
